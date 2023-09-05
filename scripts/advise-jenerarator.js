662b22bd624b14e05359cb7f7e116f9b196a906d
kai lucas <lucaskw1@s.dcsdk12.org>
2023-09-05 15:46:53 -0600

    i commit

---
+++ scripts/advise-jenerarator.js
@@ -0,0 +1,50 @@
+let btn = document.getElementById("advice-button");
+let output = document.getElementById("advice");
+
+let advisors = [
+	"A wise man once told me",
+	"Mother always says",
+	"My Uncle Rupert once said",
+];
+
+let howOften = ["never", "always", "as often as you can", "never ever"];
+
+let verbs = ["run with", "swim in", "eat", "climb", "dance with"];
+
+let adjectives = ["spiky", "furry", "funny", "spicy", "odd", "loud", "scary"];
+
+let nouns = [
+	"elephants",
+	"humans",
+	"chainsaws",
+	"nuclear bombs",
+	"computers",
+	"commuters",
+	"video games",
+];
+
+let getRandomIndex = function (a) {
+	let rand = Math.random() * a.length;
+	return Math.floor(rand);
+};
+
+let getRandomWord = function (a) {
+	return a[getRandomIndex(a)];
+};
+
+let generateAdvice = function (a, h, v, adj, n) {
+	console.log(
+		getRandomWord(a),
+		getRandomWord(h),
+		getRandomWord(v),
+		getRandomWord(adj),
+		getRandomWord(n)
+	);
+};
+
+//generateAdvice(advisors, howOften, verbs, adjectives, nouns);
+
+btn.addEventListener("click", function () {
+	//console.log("clicked!");
+	generateAdvice(advisors, howOften, verbs, adjectives, nouns);
+});
--- scripts/basics-1.js
+++ scripts/basics-1.js
@@ -1,4 +1,4 @@
-// any this I type after the slashes will be a comment
+// every thing I type after the slashes will be a comment
 
 /*
 this
@@ -9,3 +9,35 @@ lines
 */
 
 console.log("hello world");
+//keywords for declaring variables
+
+let favoritsong = 'never "gonna give you up';
+let age = 52;
+let myMoney = 50.84;
+
+
+
+
+myMoney = "bankrupt";
+
+console.log(favoritsong.length)
+
+
+let
+
+let getrandomindex=function(a) {
+    let rand = mathrandom()* a.length;
+    return math.floor(rand);
+};
+let randomDvice = getRandomWord(a)+" "+
+
+consol.log (
+    getRandomWord(a)+" "+
+    getRandomWord(h)+" "+
+    getRandomWord(v)+" "+
+    getRandomWord(adj)+" "+
+    getRandomWord(n)+" "+
+
+)
+
+//generateAdvise(advisers, howOften, verbs, adjectives, nouns);
