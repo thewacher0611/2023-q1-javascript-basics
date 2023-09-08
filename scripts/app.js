08c53c221f7b0ce5ce105463c4ad6cfc63cf7847
kai lucas <lucaskw1@s.dcsdk12.org>
2023-09-08 15:51:08 -0600

    Icommit

--- scripts/app.js
+++ scripts/app.js
@@ -5,3 +5,10 @@ const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 600;
+
+
+ctx.fillStyle = "red";
+ctx.fillRect(250, 350, 100, 100)
+
+
+
